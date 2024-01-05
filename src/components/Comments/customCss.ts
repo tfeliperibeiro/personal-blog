c62b2951298f6a76fa7c5537cb1179ec19fed69a
Thiago Ribeiro <contatotfeliperibeiro@gmail.com>
2024-01-04 22:15:26 -0300

    feature: add hyvor talk

---
+++ src/components/Comments/customCss.ts
@@ -0,0 +1,26 @@
+export const customCss = `
+.reactions-title, 
+.search-button, 
+.no-comments, 
+.comments-count, 
+.online-count, 
+.reaction-number, 
+.user-name, 
+.comment-content-inner, 
+.sort-button,
+.back {
+  color: var(--white)
+}
+
+.page-mod-button svg, .mod-button {
+  color: #b8860b
+}
+
+.notification-icon svg {
+  color: #F23D3D
+}
+
+.reaction-top img::hover {
+  cursor: none;
+}
+`;
